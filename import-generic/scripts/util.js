549f6bfceeee81027a8241c7836486438f4d9df3
mikus <mikusg@gmail.com>
2026-05-07 11:47:28 +0300

    move import logs to _log

--- import-generic/scripts/util.js
+++ import-generic/scripts/util.js
@@ -1,9 +1,14 @@
-const fs = require('fs')
+const fs = require('node:fs')
+const path = require('node:path')
+
 const col = require('ansi-colors')
 
-const LOG_FILE = `./json-importer-${new Date().toISOString().slice(0, 10)}.log`
-const logStream = fs.createWriteStream(LOG_FILE, { flags: 'a', encoding: 'utf-8' })
+const LOG_FILE_PATH = path.join(__dirname, '../_logs/')
+fs.mkdirSync(LOG_FILE_PATH, { recursive: true })
 
+const LOG_FILE = path.join(LOG_FILE_PATH, `json-importer-${new Date().toISOString().slice(0, 10)}.log`)
+//FIXME: ensure _log dir exists
+const logStream = fs.createWriteStream(LOG_FILE, { flags: 'a', encoding: 'utf-8' })
 
 async function log(params, breakBefore = false) {
   let message
