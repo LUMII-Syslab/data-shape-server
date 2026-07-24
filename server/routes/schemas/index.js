9dac670b5dc482582a0370f92750adfe33c4275e
mikus <mikusg@gmail.com>
2026-07-24 23:30:14 +0300

    dss ui - work in progress

--- server/routes/schemas/index.js
+++ server/routes/schemas/index.js
@@ -9,6 +9,9 @@ const db = require('../db')
 
 const SCHEMA_NAME_REGEX = /^[a-z0-9_]+$/
 
+const TOP_N_NODES = 12
+const TOP_N_EDGES = 7
+
 function getSchemaName(param) {
   if (!param) throw new Error("Bad schema name")
   if (typeof param !== 'string') throw new Error("Bad schema name")
@@ -32,11 +35,10 @@ async function getSchemaInfo(schemaName) {
 
     let metaPropertiesProcessed = Object.fromEntries(metaProperties.map(p => [p.name, p.textvalue ?? p.jsonvalue]).filter(p => p[1]).filter(p => !['order_inx'].includes(p[0])))
 
-    const topNclasses = await db.query(`select id, iri, display_name, cnt from ${schemaName}.classes order by cnt desc limit 10`)
-    const topNprops = await db.query(`select id, iri, display_name, cnt from ${schemaName}.properties order by cnt desc limit 10`)
+    const topNclasses = await db.query(`select id, iri, display_name, cnt from ${schemaName}.classes order by cnt desc limit ${TOP_N_NODES}`)
+    const topNprops = await db.query(`select id, iri, display_name, cnt from ${schemaName}.properties order by cnt desc limit ${TOP_N_NODES}`)
 
     let schemaInfo = Object.assign({ schemaName, classCount, propertyCount, topNclasses, topNprops }, info, metaPropertiesProcessed, )
-
 
     delete schemaInfo.id
     delete schemaInfo.endpoint_id
@@ -51,14 +53,31 @@ async function getSchemaInfo(schemaName) {
 }
 
 async function getClassInfo(schemaName, classId) {
-  const thisClass = await db.query(`select * from ${schemaName}.classes where id = $1`, classId)
+  const thisClass = (await db.query(`select * from ${schemaName}.classes where id = $1`, classId))[0]
 
-  const cc1 = await db.query(`select * from ${schemaName}.cc_rels cc join ${schemaName}.cc_rel_types t on cc.type_id = t.id where cc.class_1_id = $1 order by cc.cnt desc limit 5`, classId)
-  const cc2 = await db.query(`select * from ${schemaName}.cc_rels cc join ${schemaName}.cc_rel_types t on cc.type_id = t.id where cc.class_2_id = $1 order by cc.cnt desc limit 5`, classId)
+  const cc1_1 = await db.query(`select cc.cnt, c2.iri, c2.display_name from ${schemaName}.cc_rels cc join ${schemaName}.cc_rel_types t on cc.type_id = t.id join ${schemaName}.classes c2 on cc.class_2_id = c2.id where cc.class_1_id = $1 and cc.type_id = 1 order by cc.cnt desc limit ${TOP_N_EDGES}`, classId)
 
-  const cp = await db.query(`select * from ${schemaName}.cp_rels cp join ${schemaName}.cp_rel_types t on cp.type_id = t.id where cp.class_id = $1 order by cp.cnt desc limit 5`, classId)
+  const cc2_1 = await db.query(`select cc.cnt, c2.iri, c2.display_name from ${schemaName}.cc_rels cc join ${schemaName}.cc_rel_types t on cc.type_id = t.id join ${schemaName}.classes c2 on cc.class_1_id = c2.id where cc.class_2_id = $1 and cc.type_id = 1 order by cc.cnt desc limit ${TOP_N_EDGES}`, classId)
 
-  return { thisClass, cc1, cc2, cp }
+  const cp_1 = await db.query(`select p.iri, p.display_name, cp.cnt from ${schemaName}.cp_rels cp join ${schemaName}.cp_rel_types t on cp.type_id = t.id join ${schemaName}.properties p on cp.property_id = p.id where cp.class_id = $1 and cp.type_id = 1 order by cp.cnt desc limit ${TOP_N_EDGES}`, classId)
+  const cp_2 = await db.query(`select p.iri, p.display_name, cp.cnt from ${schemaName}.cp_rels cp join ${schemaName}.cp_rel_types t on cp.type_id = t.id join ${schemaName}.properties p on cp.property_id = p.id where cp.class_id = $1 and cp.type_id = 2 order by cp.cnt desc limit ${TOP_N_EDGES}`, classId)
+
+  return { thisClass, cc: { subclasses: cc1_1, superclasses: cc2_1 }, cp: { incoming: cp_1, outgoing: cp_2 } }
+}
+
+async function getPropertyInfo(schemaName, propertyId) {
+  const thisProperty = (await db.query(`select * from ${schemaName}.properties where id = $1`, propertyId))[0]
+
+  const pp1_1 = await db.query(`select pp.cnt, p2.iri, p2.display_name from ${schemaName}.pp_rels pp join ${schemaName}.pp_rel_types t on pp.type_id = t.id join ${schemaName}.properties p2 on pp.property_2_id = p2.id where pp.property_1_id = $1 and pp.type_id = 1 order by pp.cnt desc limit ${TOP_N_EDGES}`, propertyId)
+  const pp2_1 = await db.query(`select pp.cnt, p2.iri, p2.display_name from ${schemaName}.pp_rels pp join ${schemaName}.pp_rel_types t on pp.type_id = t.id join ${schemaName}.properties p2 on pp.property_1_id = p2.id where pp.property_2_id = $1 and pp.type_id = 1 order by pp.cnt desc limit ${TOP_N_EDGES}`, propertyId)
+
+  const pp_2 = await db.query(`select pp.cnt, p2.iri, p2.display_name from ${schemaName}.pp_rels pp join ${schemaName}.pp_rel_types t on pp.type_id = t.id join ${schemaName}.properties p2 on pp.property_2_id = p2.id where pp.property_1_id = $1 and pp.type_id = 2 order by pp.cnt desc limit ${TOP_N_EDGES}`, propertyId)
+  const pp_3 = await db.query(`select pp.cnt, p2.iri, p2.display_name from ${schemaName}.pp_rels pp join ${schemaName}.pp_rel_types t on pp.type_id = t.id join ${schemaName}.properties p2 on pp.property_2_id = p2.id where pp.property_1_id = $1 and pp.type_id = 3 order by pp.cnt desc limit ${TOP_N_EDGES}`, propertyId)
+
+  const cp_1 = await db.query(`select c.iri, c.display_name, cp.cnt from ${schemaName}.cp_rels cp join ${schemaName}.cp_rel_types t on cp.type_id = t.id join ${schemaName}.classes c on cp.class_id = c.id where cp.property_id = $1 and cp.type_id = 1 order by cp.cnt desc limit ${TOP_N_EDGES}`, propertyId)
+  const cp_2 = await db.query(`select c.iri, c.display_name, cp.cnt from ${schemaName}.cp_rels cp join ${schemaName}.cp_rel_types t on cp.type_id = t.id join ${schemaName}.classes c on cp.class_id = c.id where cp.property_id = $1 and cp.type_id = 2 order by cp.cnt desc limit ${TOP_N_EDGES}`, propertyId)
+
+  return { thisProperty, pp: { followed_by: pp1_1, follows: pp2_1, common_subject: pp_2, common_object: pp_3}, cp: { goes_to: cp_1, comes_from: cp_2 } }
 }
 
 router.get('/:schema/classes/:id', async (req, res, next) => {
@@ -66,9 +85,19 @@ router.get('/:schema/classes/:id', async (req, res, next) => {
   const classId = getId(req.params.id)
 
   const classInfo = await getClassInfo(schemaName, classId)
-  const className = classInfo.display_name
+  const className = classInfo.thisClass.display_name
 
   res.render('class', { schemaName, className, classInfo })
+})
+
+router.get('/:schema/properties/:id', async (req, res, next) => {
+  const schemaName = getSchemaName(req.params.schema)
+  const propertyId = getId(req.params.id)
+
+  const propertyInfo = await getPropertyInfo(schemaName, propertyId)
+  const propertyName = propertyInfo.thisProperty.display_name
+
+  res.render('property', { schemaName, propertyName, propertyInfo })
 })
 
 router.get('/:schema', async (req, res, next) => {
